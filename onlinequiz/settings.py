79a7848da9a44e4b1fefe819bdbda4ce9d09b987
ayodele <niran360@gmail.com>
2023-12-13 14:47:05 +0100

    config dep

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -125,11 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_STORAGE = 'whitenise.storage.CompressedStaticFileaStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-STATIC_DIR,
+os.path.join(BASE_DIR, 'static')
  ]
 
 LOGIN_REDIRECT_URL='/afterlogin'
